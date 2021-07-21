2ac3ee62be20594a93ca130b7299d0c8d400d31f
Harish Durga <harishdurga@LAPTOP-QCJUC3FA.localdomain>
2021-07-21 15:02:57 +0530

    removed db transaction from migration file

--- database/migrations/2021_05_22_053359_create_quizzes_table.php
+++ database/migrations/2021_05_22_053359_create_quizzes_table.php
@@ -19,122 +19,114 @@ class CreateQuizzesTable extends Migration
      */
     public function up()
     {
-        DB::beginTransaction();
-        try {
-            //Topics Table
-            Schema::create($this->tableNames['topics'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('topic');
-                $table->string('slug')->unique();
-                $table->unsignedBigInteger('parent_id')->nullable();
-                $table->boolean('is_active')->default(true);
-                $table->timestamps();
-                $table->foreign('parent_id')->references('id')->on($this->tableNames['topics'])->onDelete('cascade');
-            });
+        //Topics Table
+        Schema::create($this->tableNames['topics'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('topic');
+            $table->string('slug')->unique();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->foreign('parent_id')->references('id')->on($this->tableNames['topics'])->onDelete('cascade');
+        });
 
-            //Question Types Table
-            Schema::create($this->tableNames['question_types'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('question_type');
-                $table->timestamps();
-            });
+        //Question Types Table
+        Schema::create($this->tableNames['question_types'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question_type');
+            $table->timestamps();
+        });
 
-            //Questions Table
-            Schema::create($this->tableNames['questions'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->text('question');
-                $table->unsignedBigInteger('question_type_id');
-                $table->text('media_url')->nullable();
-                $table->string('media_type')->nullable();
-                $table->boolean('is_active')->default(true);
-                $table->timestamps();
-                $table->foreign('question_type_id')->references('id')->on($this->tableNames['question_types'])->onDelete('cascade');
-            });
+        //Questions Table
+        Schema::create($this->tableNames['questions'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('question');
+            $table->unsignedBigInteger('question_type_id');
+            $table->text('media_url')->nullable();
+            $table->string('media_type')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->foreign('question_type_id')->references('id')->on($this->tableNames['question_types'])->onDelete('cascade');
+        });
 
-            //Quiz, Questions and Topics Relations Table
-            Schema::create($this->tableNames['topicables'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedBigInteger('topic_id');
-                $table->unsignedBigInteger('topicable_id');
-                $table->string('topicable_type');
-                $table->timestamps();
-                $table->foreign('topic_id')->references('id')->on($this->tableNames['topics'])->onDelete('cascade');
-            });
+        //Quiz, Questions and Topics Relations Table
+        Schema::create($this->tableNames['topicables'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('topic_id');
+            $table->unsignedBigInteger('topicable_id');
+            $table->string('topicable_type');
+            $table->timestamps();
+            $table->foreign('topic_id')->references('id')->on($this->tableNames['topics'])->onDelete('cascade');
+        });
 
-            //Question Options Table
-            Schema::create($this->tableNames['question_options'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedBigInteger('question_id');
-                $table->string('option')->nullable();
-                $table->string('media_url')->nullable();
-                $table->string('media_type')->nullable();
-                $table->boolean('is_correct')->default(false);
-                $table->timestamps();
-                $table->foreign('question_id')->references('id')->on($this->tableNames['questions'])->onDelete('cascade');
-            });
+        //Question Options Table
+        Schema::create($this->tableNames['question_options'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('question_id');
+            $table->string('option')->nullable();
+            $table->string('media_url')->nullable();
+            $table->string('media_type')->nullable();
+            $table->boolean('is_correct')->default(false);
+            $table->timestamps();
+            $table->foreign('question_id')->references('id')->on($this->tableNames['questions'])->onDelete('cascade');
+        });
 
-            //Quizzes Table
-            Schema::create($this->tableNames['quizzes'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('title');
-                $table->string('slug')->unique();
-                $table->text('description')->nullable();
-                $table->float('total_marks')->default(0); //0 means no marks
-                $table->float('pass_marks')->default(0); //0 means no pass marks
-                $table->unsignedInteger('max_attempts')->default(0); //0 means unlimited attempts
-                $table->tinyInteger('is_published')->default(0); //0 means not published, 1 means published
-                $table->string('media_url')->nullable(); //Can be used for cover image, logo etc.
-                $table->string('media_type')->nullable(); //image,video,audio etc.
-                $table->unsignedInteger('duration')->default(0); //0 means no duration
-                $table->timestamp('valid_from')->default(now());
-                $table->timestamp('valid_upto')->nullable(); //null means no expiry
-                $table->unsignedInteger('time_between_attempts')->default(0); //0 means no time between attempts, immediately
-                $table->timestamps();
-            });
+        //Quizzes Table
+        Schema::create($this->tableNames['quizzes'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->float('total_marks')->default(0); //0 means no marks
+            $table->float('pass_marks')->default(0); //0 means no pass marks
+            $table->unsignedInteger('max_attempts')->default(0); //0 means unlimited attempts
+            $table->tinyInteger('is_published')->default(0); //0 means not published, 1 means published
+            $table->string('media_url')->nullable(); //Can be used for cover image, logo etc.
+            $table->string('media_type')->nullable(); //image,video,audio etc.
+            $table->unsignedInteger('duration')->default(0); //0 means no duration
+            $table->timestamp('valid_from')->default(now());
+            $table->timestamp('valid_upto')->nullable(); //null means no expiry
+            $table->unsignedInteger('time_between_attempts')->default(0); //0 means no time between attempts, immediately
+            $table->timestamps();
+        });
 
-            //Quiz Questions Table
-            Schema::create($this->tableNames['quiz_questions'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedBigInteger('quiz_id');
-                $table->unsignedBigInteger('question_id');
-                $table->unsignedFloat('marks')->default(0); //0 means no marks
-                $table->unsignedFloat('negative_marks')->default(0); //0 means no negative marks in case of wrong answer
-                $table->boolean('is_optional')->default(false); //0 means not optional, 1 means optional
-                $table->unsignedInteger('order')->default(0);
-                $table->timestamps();
-                $table->foreign('quiz_id')->references('id')->on($this->tableNames['quizzes'])->onDelete('cascade');
-                $table->foreign('question_id')->references('id')->on($this->tableNames['questions'])->onDelete('cascade');
-                $table->unique(['quiz_id', 'question_id']);
-            });
+        //Quiz Questions Table
+        Schema::create($this->tableNames['quiz_questions'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('quiz_id');
+            $table->unsignedBigInteger('question_id');
+            $table->unsignedFloat('marks')->default(0); //0 means no marks
+            $table->unsignedFloat('negative_marks')->default(0); //0 means no negative marks in case of wrong answer
+            $table->boolean('is_optional')->default(false); //0 means not optional, 1 means optional
+            $table->unsignedInteger('order')->default(0);
+            $table->timestamps();
+            $table->foreign('quiz_id')->references('id')->on($this->tableNames['quizzes'])->onDelete('cascade');
+            $table->foreign('question_id')->references('id')->on($this->tableNames['questions'])->onDelete('cascade');
+            $table->unique(['quiz_id', 'question_id']);
+        });
 
-            //Quiz Attempts Table
-            Schema::create($this->tableNames['quiz_attempts'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedBigInteger('quiz_id');
-                $table->unsignedBigInteger('participant_id');
-                $table->string('participant_type');
-                $table->timestamps();
-                $table->foreign('quiz_id')->references('id')->on($this->tableNames['quizzes'])->onDelete('cascade');
-            });
+        //Quiz Attempts Table
+        Schema::create($this->tableNames['quiz_attempts'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('quiz_id');
+            $table->unsignedBigInteger('participant_id');
+            $table->string('participant_type');
+            $table->timestamps();
+            $table->foreign('quiz_id')->references('id')->on($this->tableNames['quizzes'])->onDelete('cascade');
+        });
 
-            //Quiz Attempt Answers Table
-            Schema::create($this->tableNames['quiz_attempt_answers'], function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedBigInteger('quiz_attempt_id');
-                $table->unsignedBigInteger('quiz_question_id');
-                $table->unsignedBigInteger('question_option_id');
-                $table->string('answer')->nullable();
-                $table->timestamps();
-                $table->foreign('quiz_attempt_id')->references('id')->on($this->tableNames['quiz_attempts'])->onDelete('cascade');
-                $table->foreign('quiz_question_id')->references('id')->on($this->tableNames['quiz_questions'])->onDelete('cascade');
-                $table->foreign('question_option_id')->references('id')->on($this->tableNames['question_options'])->onDelete('cascade');
-            });
-
-            DB::commit();
-        } catch (\Throwable $th) {
-            DB::rollBack();
-            \Log::error($th);
-        }
+        //Quiz Attempt Answers Table
+        Schema::create($this->tableNames['quiz_attempt_answers'], function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('quiz_attempt_id');
+            $table->unsignedBigInteger('quiz_question_id');
+            $table->unsignedBigInteger('question_option_id');
+            $table->string('answer')->nullable();
+            $table->timestamps();
+            $table->foreign('quiz_attempt_id')->references('id')->on($this->tableNames['quiz_attempts'])->onDelete('cascade');
+            $table->foreign('quiz_question_id')->references('id')->on($this->tableNames['quiz_questions'])->onDelete('cascade');
+            $table->foreign('question_option_id')->references('id')->on($this->tableNames['question_options'])->onDelete('cascade');
+        });
     }
 
     /**
