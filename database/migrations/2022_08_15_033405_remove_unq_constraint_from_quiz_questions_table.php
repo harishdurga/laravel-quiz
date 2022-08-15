50d8e56c38188591d1604e59653c9e5f28ecfc14
Harish Durga <durgaharish5@gmail.com>
2022-08-15 10:26:10 +0530

    26 migration file to drop unique constraint

--- database/migrations/2022_08_15_033405_remove_unq_constraint_from_quiz_questions_table.php
+++ database/migrations/2022_08_15_033405_remove_unq_constraint_from_quiz_questions_table.php
@@ -21,8 +21,13 @@ return new class extends Migration
         Schema::table($this->tableNames['quiz_questions'], function (Blueprint $table) {
             $sm = Schema::getConnection()->getDoctrineSchemaManager();
             $indexesFound = $sm->listTableIndexes($this->tableNames['quiz_questions']);
-            if (array_key_exists($this->tableNames['quiz_questions'] . "_quiz_id_question_id_unique", $indexesFound))
+            if (array_key_exists($this->tableNames['quiz_questions'] . "_quiz_id_question_id_unique", $indexesFound)) {
+                $table->dropForeign($this->tableNames['quiz_questions'] . '_question_id_foreign');
+                $table->dropForeign($this->tableNames['quiz_questions'] . '_quiz_id_foreign');
                 $table->dropUnique($this->tableNames['quiz_questions'] . "_quiz_id_question_id_unique");
+                $table->foreignId('quiz_id')->change()->constrained($this->tableNames['quizzes'])->cascadeOnDelete();
+                $table->foreignId('question_id')->change()->constrained($this->tableNames['questions'])->cascadeOnDelete();
+            }
         });
     }
 
